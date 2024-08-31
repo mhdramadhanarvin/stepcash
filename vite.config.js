6326ff4ca6c4f232c51fd537dd8796bca7646e6d
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-08-31 17:51:25 +0700

    feat: add manifest

--- vite.config.js
+++ vite.config.js
@@ -27,12 +27,12 @@ export default defineConfig({
                 theme_color: "#ffffff",
                 icons: [
                     {
-                        src: "icon-192x192.png",
+                        src: "/icons/icon-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
                     },
                     {
-                        src: "icon-512x512.png",
+                        src: "/icons/icon-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                     },

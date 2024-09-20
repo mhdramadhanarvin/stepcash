4949b442c8c333cbb32b5ba48ca8e4ddac4326b6
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-09-21 01:34:24 +0700

    fix: improve cd

--- decrypt_env.sh
+++ decrypt_env.sh
@@ -5,4 +5,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./storage/app/credentials.json ./credentials.json.gpg
+--output ./.env ./.env.gpg

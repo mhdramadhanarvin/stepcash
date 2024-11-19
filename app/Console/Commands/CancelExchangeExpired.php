e2e149733eaa195cfa8a9ea6e344ba8c6bc37b51
Mhd Ramadhan Arvin <38186144+mhdramadhanarvin@users.noreply.github.com>
2024-11-19 20:47:58 +0700

    feat: add job to auto cancel expired reward claims (#27)

---
+++ app/Console/Commands/CancelExchangeExpired.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\NotificationEnum;
+use App\Enums\RewardClaimEnum;
+use App\Notifications\UserNotification;
+use App\Repositories\CoinHistoryRepositoryInterface;
+use App\Repositories\RewardClaimRepositoryInterface;
+use App\Repositories\RewardRepositoryInterface;
+use App\Repositories\UserRepositoryInterface;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class CancelExchangeExpired extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:cancel-exchange-expired';
+    protected $rewardRepository;
+    protected $rewardClaimRepository;
+    protected $coinHistoryRepository;
+    protected $userRepository;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    public function __construct(
+        RewardRepositoryInterface $rewardRepository,
+        RewardClaimRepositoryInterface $rewardClaimRepository,
+        CoinHistoryRepositoryInterface $coinHistoryRepository,
+        UserRepositoryInterface $userRepository
+    ) {
+        parent::__construct();
+        $this->rewardRepository = $rewardRepository;
+        $this->rewardClaimRepository = $rewardClaimRepository;
+        $this->coinHistoryRepository = $coinHistoryRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Starting Cancel Reward Claim Expired...');
+
+        DB::beginTransaction();
+        try {
+            $rewardClaim = $this->rewardClaimRepository;
+            $rewardClaim->setWithRelation(['reward']);
+            $rewardClaim->setWhereArg([
+                ['created_at', '<=', now()->subDay()],
+                ['status', '=', RewardClaimEnum::WAITING_CONFIRMATION]
+            ]);
+            foreach ($rewardClaim->getAll() as $claim) {
+                $this->rewardClaimRepository->update(['status' => RewardClaimEnum::REJECTED], $claim['id']);
+                $user = $this->userRepository->getById($claim['user_id']);
+                $this->userRepository->addCoin($user, $claim['price']);
+                $user->notify(new UserNotification(
+                    NotificationEnum::EXCHANGE_CANCELED,
+                    "Penukaran produk " . $claim['reward']['title'] . " telah kadaluarsa, " . $claim['price'] . " coin telah dikembalikan."
+                ));
+                $this->info("Reward Claim with ID " . $claim['id']. " has canceled");
+            }
+
+            $this->info('Successfully Cancel Reward Claim Expired.');
+            DB::commit();
+        } catch (\Exception $e) {
+            $this->info("ERROR Cancel Reward Claim Expired");
+            Log::error($e->getMessage());
+            DB::rollback();
+        }
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('queue:work --stop-when-empty --queue=high,default')->everyMinute()->withoutOverlapping()->sendOutputTo(getcwd()."/queue.log");
 Schedule::command('queue:work --stop-when-empty --queue=stepsCount')->everyFiveMinutes()->withoutOverlapping()->sendOutputTo(getcwd()."/queue.log");
 Schedule::command('app:convert-steps-to-coin')->dailyAt('00:00')->withoutOverlapping()->sendOutputTo(getcwd()."/converter.log");
+Schedule::command('app:cancel-exchange-expired')->hourly()->withoutOverlapping()->sendOutputTo(getcwd()."/converter.log");
